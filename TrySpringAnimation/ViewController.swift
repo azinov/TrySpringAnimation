71908cb060abe12fa13c7c720c90c3b9d508c4c7
Alexey Zinovyev <zinovkin@gmail.com>
2023-06-13 22:48:52 +0300

    Initial Commit

---
+++ TrySpringAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift (5.0)
+//  TrySpringAnimation
+//  
+//  Created by Alexey Zinoviev on 13.06.2023
+//
+	
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
