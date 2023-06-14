f52d603bf58bc8d9b1047c64d69e99af2c26a885
Alexey Zinovyev <zinovkin@gmail.com>
2023-06-14 10:03:07 +0300

    no mvc complete

--- TrySpringAnimation/ViewController.swift
+++ TrySpringAnimation/ViewController.swift
@@ -7,14 +7,40 @@
 	
 
 import UIKit
+import SpringAnimation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var animationLabel: SpringLabel!
+    
+    private func randomPreset<T>(_ Enum: T.Type) -> String where T: CaseIterable & RawRepresentable {
+        let index = Int.random(in: 0..<Enum.allCases.count - 1)
+        return Enum.allCases[index as! T.AllCases.Index].rawValue as! String
     }
-
-
+    
+    @IBAction func startAnimationButton(_ sender: SpringButton) {
+        animationView.animation = randomPreset(AnimationPreset.self)
+        animationView.curve = randomPreset(AnimationCurve.self)
+        animationView.force = CGFloat.random(in: 1...3)
+        animationView.delay = CGFloat.random(in: 0...2)
+        animationView.duration = CGFloat.random(in: 0.5...2)
+        animationView.animate()
+        
+        animationLabel.text =
+            """
+            preset: \(animationView.animation)
+            curve: \(animationView.curve)
+            force: \(String(format: "%.1f", animationView.force))
+            duration: \(String(format: "%.1f", animationView.duration))
+            delay: \(String(format: "%.1f", animationView.delay))
+            """
+        
+        sender.animation = "pop"
+        sender.animate()
+        
+        
+    }
 }
+
 
